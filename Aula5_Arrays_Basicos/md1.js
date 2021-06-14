9ff7343fc34ed00ef49ac8cc8410a9f22fe51104
Henrique <henrique.empresa53@outlook.com>
2021-06-13 23:24:03 -0300

    Adicionando pasta da aula 5 e microdesafio correspondente

---
+++ Aula5_Arrays_Basicos/md1.js
@@ -0,0 +1,23 @@
+let compras = ["pao","ovos","leite"];
+
+//Join
+console.log("O método Join faz a junção dos elementos de um array usando o separador que especificarmos e retorna uma string com os elementos juntos");
+console.log(compras.join(" - "));
+
+
+//Pop
+console.log("O método Pop remove o último elemento de um array e retorna o elemento removido");
+console.log(compras.pop());
+
+
+//Push
+console.log("O método Push acrescenta um elemento ou vários elementos ao final do array e retorna o novo comprimento do array");
+console.log(compras.push("leite","farinha"));
+
+//Shift
+console.log("O método Shift remove o primeiro elemento de um array e retorna o elemento removido");
+console.log(compras.shift());
+
+//Unshift
+console.log("O método Unshift acrescenta um elemento ou mais no início de um array e retorna o novo comprimento do array");
+console.log(compras.unshift("pao","açucar"));
