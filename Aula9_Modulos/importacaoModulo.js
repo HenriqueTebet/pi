1ca1d10fb789587237cc834f60df467557a2bad0
Henrique <henrique.empresa53@outlook.com>
2021-06-25 13:19:28 -0300

    Adicionando pasta da aula 9 e microdesafio correspondente

---
+++ Aula9_Modulos/importacaoModulo.js
@@ -0,0 +1,6 @@
+//Importação do módulo e execução da função importada
+
+const importacaoModulo = require("./importacaoModulo");
+const primeiroModulo = require("./importacaoModulo/primeiroModulo");
+
+primeiroModulo();
---
+++ Aula9_Modulos/importacaoModulo/primeiroModulo.js
@@ -0,0 +1,7 @@
+//Função que será exportada
+
+const primeiroModulo = () => console.log("Olá, Mundo! Estou aprendendo módulos.");
+
+module.exports = primeiroModulo;
+
+
